a47d08ffc8a50604d1ff9e7e4cb6cd52c3fb0cd6
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-04-14 12:11:00 +0100

    Reduced delays - tested

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
-setTimeout(function(){
-    console.log("Time: 5s");
-    }, 5000
-    );
+//setTimeout(function(){
+//    console.log("Time: 5s");
+//    }, 5000
+//    );
 
 setTimeout(function(){
     console.log("Time: 10s");
@@ -15,37 +15,7 @@ setTimeout(function(){
       console.log("Disconnected");
     });
 
-    port.postMessage({ text: "Hello, my_application. you boii" });
+    port.postMessage({ text: "Shares in Boeing fell" });
     console.log("Attempted to send to host.")
-    }, 10000
-    );
-
-
-setTimeout(function(){
-    console.log("Time: 15s");
-    }, 15000
-    );
-
-
-setTimeout(function(){
-    console.log("Time: 20s");
-    var port = chrome.runtime.connectNative('host_manifest');
-
-    port.onMessage.addListener(function(msg) {
-      console.log("Received: " + msg.text);
-    });
-
-    port.onDisconnect.addListener(function() {
-      console.log("Disconnected");
-    });
-
-    port.postMessage({ text: "Hello, my_application" });
-    console.log("Attempted to send to host.")
-    }, 20000
-    );
-
-
-setTimeout(function(){
-    console.log("Time: 25s");
-    }, 25000
+    }, 2000
     );
