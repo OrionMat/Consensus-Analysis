5e4f6c5232282e1e4822befebaac92b60cba1d21
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-04-14 19:14:32 +0100

    working context menu selector

--- extension/background.js
+++ extension/background.js
@@ -1,21 +1,33 @@
+var contextMenuItem = {
+  "id" : "check_statement",
+  "title" : "Check Statement",
+  "contexts" : ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem)
+
+function is_valid_statement(text){
+  // checks text is valid
+  return true
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickedData){
+  if (clickedData.menuItemId == "check_statement" && clickedData.selectionText){
+    if (is_valid_statement(clickedData.selectionText)){
+      var port = chrome.runtime.connectNative('host_manifest'); // runs python script
+      port.onMessage.addListener(function(msg) {
+        console.log("Received: " + msg.text);
+      });
+      port.onDisconnect.addListener(function() {
+        console.log("Disconnected");
+      });
+      port.postMessage({ text: clickedData.selectionText });
+      console.log("Attempted to send to host.")
+    }
+  }
+})
+
 //setTimeout(function(){
 //    console.log("Time: 5s");
 //    }, 5000
 //    );
-
-setTimeout(function(){
-    console.log("Time: 10s");
-    var port = chrome.runtime.connectNative('host_manifest'); // runs python script
-
-    port.onMessage.addListener(function(msg) {
-      console.log("Received: " + msg.text);
-    });
-
-    port.onDisconnect.addListener(function() {
-      console.log("Disconnected");
-    });
-
-    port.postMessage({ text: "Shares in Boeing fell" });
-    console.log("Attempted to send to host.")
-    }, 2000
-    );
