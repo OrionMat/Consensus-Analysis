728cea9facbc06e393e3d7f05dd762631ad78183
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-05-25 18:32:21 +0100

    deleted comments

--- host/data_saving.py
+++ host/data_saving.py
@@ -56,29 +56,3 @@ def lists_to_dictList(agency, title_list, date_list, article_list, url_list):
 
 
 
-
-
-
-# deal with this: (format, add getting dates and links to corresponding functions)
-
-#nyt_opinion_url = 'https://www.nytimes.com/section/opinion'
-#nyt_world_url = 'https://www.nytimes.com/section/world'
-
-#NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list, NYTscrap_opinion_sentence_sentiments, NYTscrap_opinion_article_sentiments = NSA.NYT_scrape_SA(nyt_opinion_url)
-#NYTscrap_world_title_list, NYTscrap_world_sentence_list, NYTscrap_world_sentence_sentiments, NYTscrap_world_article_sentiments = NSA.NYT_scrape_SA(nyt_world_url)
-
-#news_dicList = lists_to_dictList('NYT opinion', NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list)
-#print(news_dicList)
-#news_dictionary = dict(zip(NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list))
-
-
-
-
-
-#csv_file_path = 'consensus_data.csv'
-#csv_columns = ['agency','title', 'date', 'article', 'link']
-
-#initiate_csv(csv_file_path, csv_columns)                 # use first time 
-#append_csv(csv_file_path, csv_columns, news_dicList)     # use rest of the time
-
-
