f9d42d3ead749666694200b71ebc18b30cf7ad6e
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-05-25 08:26:54 +0100

    results cleared, notification at right time

--- extension/background.js
+++ extension/background.js
@@ -14,6 +14,18 @@ chrome.contextMenus.onClicked.addListener(function(clickedData){
   if (clickedData.menuItemId == "check_statement" && clickedData.selectionText){
     if (is_valid_statement(clickedData.selectionText)){
 
+      // clear previouse results
+      chrome.storage.sync.set({'num_Articles': '0'}); 
+      chrome.storage.sync.set({'agencies': '0'});
+      chrome.storage.sync.set({'titles' : '0'});
+      chrome.storage.sync.set({'dates' : '0'});
+      chrome.storage.sync.set({'urls' : '0'});
+      chrome.storage.sync.set({'results' : '0'})
+
+      // chrome.tabs.executeScript({
+      //   file: 'contentScript.js'
+      // });
+
       chrome.storage.sync.set({'statement': clickedData.selectionText});
       var port = chrome.runtime.connectNative('host_manifest'); // runs python script
 
@@ -36,6 +48,14 @@ chrome.contextMenus.onClicked.addListener(function(clickedData){
         if(msg.name == "articleResults"){
           console.log(msg.results)
           chrome.storage.sync.set({"results" : msg.results})
+          // create a notification
+          var notifOptions = {
+            type: "basic",
+            iconUrl: "images/blueTick.png",
+            title: "Done!",
+            message: "The results are in, check em out legend."
+          };
+          chrome.notifications.create('doneNotif', notifOptions);
         }
       });
 
@@ -48,14 +68,7 @@ chrome.contextMenus.onClicked.addListener(function(clickedData){
   }
 });
 
-chrome.storage.onChanged.addListener(function(changes, storageName){
-  // create a notification
-  var notifOptions = {
-    type: "basic",
-    iconUrl: "images/blueTick.png",
-    title: "Done!",
-    message: "The results are in, check em out."
-  };
-  chrome.notifications.create('doneNotif', notifOptions);
-  //alert("Done!")
-});
+// chrome.storage.onChanged.addListener(function(changes, storageName){
+
+//   //alert("Done!")
+// });
